86f36108e80f3afa4d8f11f0bcd187f8213a95f7
Haarni Atte J <atte.h.1998@gmail.com>
2018-01-28 19:13:09 +0200

    Puhelinluettelon haku

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
 
+const SubmitButton = ({ name }) => (
+  <button type="submit">{name}</button>
+)
+
+
+const Form = (props) => (
+  <form onSubmit={props.onSubmit}>
+    <input onChange={props.onChange} />
+    <SubmitButton name={props.buttonName} />
+  </form>
+)
+
+const Persons = ({ persons, filter }) => {
+  console.log(filter)
+  if (true) {
+    return (
+      <div>
+        {persons.filter(p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1).map(f => <p key={f.id}>{f.name + ' ' + f.number}</p>)}
+      </div>
+    )
+  }
+}
+
+
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       persons: [
-        {
-          id: 1,
-          name: 'Arto Hellas'
-        }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Martti Tienari', number: '040-123456', id: 2 },
+        { name: 'Arto Järvinen', number: '040-123456', id: 3 },
+        { name: 'Lea Kutvonen', number: '040-123456', id: 4 }
+
       ],
-      newName: 'lul'
+      filtered: [],
+      newName: '',
+      newNumber: '',
+      filter: '',
+      filterText: ''
     }
   }
 
@@ -18,10 +48,29 @@ class App extends React.Component {
     this.setState({ newName: event.target.value })
   }
 
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
+  }
+
+  handleFilter = (event) => {
+
+    this.setState({ filterText: event.target.value })
+  }
+
+  filterNumber = (event) => {
+    event.preventDefault()
+
+    /*const persons = this.state.persons
+    const filtered = persons.filter(p => p.name.toLowerCase().indexOf(this.state.filter.toLowerCase()) != -1) */
+    this.setState({ filter: this.state.filterText })
+  }
+
+
   addNumber = (event) => {
     event.preventDefault()
     const nameObject = {
       name: this.state.newName,
+      number: this.state.newNumber,
       id: this.state.persons.length + 1
     }
     const contains = this.state.persons.some(p =>
@@ -32,22 +81,37 @@ class App extends React.Component {
     if (!contains) {
       this.setState({
         persons: persons,
-        newName: ''
+        newName: '',
+        newNumber: '',
       })
     } else {
-      this.setState({ newName: '' })
+      this.setState({ newName: '', newNumber: '' })
     }
   }
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
+        <Form
+          onSubmit={this.filterNumber}
+          onChange={this.handleFilter}
+          buttonName={"hae"}
+        />
+        <h3>Lisää uusi</h3>
         <form onSubmit={this.addNumber}>
           <div>
             nimi:
             <input
               value={this.state.newName}
+              onSubmit={this.filterNumber}
               onChange={this.handleNameChange}
+            />
+          </div>
+          <div>
+            numero:
+            <input
+              value={this.state.newNumber}
+              onChange={this.handleNumberChange}
             />
           </div>
           <div>
@@ -55,7 +119,7 @@ class App extends React.Component {
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(person => <p key={person.id}>{person.name}</p>)}
+        <Persons persons={this.state.persons} filter={this.state.filter} />
       </div>
     )
   }
