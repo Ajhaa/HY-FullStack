5ee97dbb202e66e2549acc191894b641813683be
Haarni Atte J <atte.h.1998@gmail.com>
2018-02-01 14:24:48 +0200

    refaktorointia

--- osa_2/puhelinluettelo/src/App.js
+++ osa_2/puhelinluettelo/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const SubmitButton = ({ name }) => (
-  <button type="submit">{name}</button>
+const Button = ({ name, type }) => (
+  <button type={type}>{name}</button>
 )
 
-
-const Form = (props) => (
-  <form onSubmit={props.onSubmit}>
-    <input onChange={props.onChange} />
-    <SubmitButton name={props.buttonName} />
-  </form>
+const Input = ({ name, onChange, value }) => (
+  <div>
+    {name}:
+    <input value={value} onChange={onChange} />
+  </div>
 )
 
 const Persons = ({ persons, filter }) => (
@@ -17,8 +16,6 @@ const Persons = ({ persons, filter }) => (
     {persons.filter(p => p.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1).map(f => <p key={f.id}>{f.name + ' ' + f.number}</p>)}
   </div>
 )
-
-
 
 
 class App extends React.Component {
@@ -51,6 +48,9 @@ class App extends React.Component {
 
     this.setState({ filterText: event.target.value })
   }
+  handleChange = (event, t) => {
+    this.setState({ [t]: event.target.value })
+  }
 
   filterNumber = (event) => {
     event.preventDefault()
@@ -68,7 +68,7 @@ class App extends React.Component {
     }
     const nameExists = () => (
       this.state.persons.some(p =>
-      p.name.toLowerCase() === this.state.newName.toLowerCase())
+        p.name.toLowerCase() === this.state.newName.toLowerCase())
     )
 
     const persons = this.state.persons.concat(nameObject)
@@ -86,31 +86,21 @@ class App extends React.Component {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <Form
-          onSubmit={this.filterNumber}
-          onChange={this.handleFilter}
-          buttonName={"hae"}
-        />
+        <input onChange={(e) => this.handleChange(e, 'filterText')} />
+        <button type="button" onClick={this.filterNumber}>hae</button>
         <h3>Lisää uusi</h3>
         <form onSubmit={this.addNumber}>
-          <div>
-            nimi:
-            <input
-              value={this.state.newName}
-              onSubmit={this.filterNumber}
-              onChange={this.handleNameChange}
-            />
-          </div>
-          <div>
-            numero:
-            <input
-              value={this.state.newNumber}
-              onChange={this.handleNumberChange}
-            />
-          </div>
-          <div>
-            <button type="submit">lisää</button>
-          </div>
+          <Input
+            name={'nimi'}
+            value={this.state.newName}
+            onChange={(e) => this.handleChange(e, 'newName')}
+          />
+          <Input
+            name={'numero'}
+            value={this.state.newNumber}
+            onChange={(e) => this.handleChange(e, 'newNumber')}
+          />
+          <Button name={'lisää'} type={'submit'} />
         </form>
         <h2>Numerot</h2>
         <Persons persons={this.state.persons} filter={this.state.filter} />
